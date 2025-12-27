da99e87af4a4d8521d60b99c16f5c37b746ec172
axrorback <axacyber1@gmail.com>
2025-12-27 22:04:48 +0500

    switches to dotenv for secret key management

--- chessGame/settings.py
+++ chessGame/settings.py
@@ -18,9 +18,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
-
+from dotenv import load_dotenv
+load_dotenv()
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-zvlz@7#pb536%23^a9z&g5-$)mx$6*2d+wo47swt2hu@9q(p2#'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
